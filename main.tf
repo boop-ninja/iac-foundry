ed5cf90f3491b30d9fbe82ea4edc388f3dd9811f
mbround18 <michael.bruno1337@gmail.com>
2024-08-09 12:54:59 -0700

    Directory Breakout

--- main.tf
+++ main.tf
@@ -1,79 +1,41 @@
 locals {
-  namespace = kubernetes_namespace.i.metadata[0].name
-  common_labels = {
-    app     = local.app_name_safe
-    hosting = "foundry-vtt"
-  }
-  ports = {
-    web = {
-      name = "web"
-      port = 4444
+  namespace     = kubernetes_namespace.i.metadata[0].name
+  common_labels = { app = local.app_name_safe, hosting = "foundry-vtt" }
+  ports         = { web = { name = "web", port = 4444 }, syncthing = { name = "syncthing", port = 8384 } }
+
+  volume_mounts = [
+    {
+      name       = "foundry_app"
+      mount_path = "/foundryvtt"
+      sub_path   = ""
+    },
+    {
+      name       = "core"
+      mount_path = "/foundrydata/"
+      sub_path   = ""
+    },
+    {
+      name       = "backups"
+      mount_path = "/foundrydata/Backups"
+      sub_path   = ""
+    },
+    {
+      name       = "config"
+      mount_path = "/foundrydata/Config"
+      sub_path   = ""
+    },
+    {
+      name       = "data"
+      mount_path = "/foundrydata/Data"
+      sub_path   = ""
     }
-    syncthing = {
-      name = "syncthing"
-      port = 8384
-    }
-  }
-  volumes = {
-    foundry_data = {
-      name = "foundry-data"
-      path = "/foundrydata"
-      size = "10Gi"
-    }
-    foundry_app = {
-      name = "foundry-app"
-      path = "/foundryvtt"
-      size = "2Gi"
-    }
-  }
+  ]
 }
 
 resource "kubernetes_namespace" "i" {
   metadata {
     name   = local.app_name_safe
     labels = local.common_labels
-  }
-}
-
-resource "kubernetes_persistent_volume_claim" "o" {
-  depends_on = [kubernetes_namespace.i]
-  metadata {
-    name      = local.app_name_safe
-    namespace = local.namespace
-    labels    = local.common_labels
-  }
-  spec {
-    storage_class_name = "longhorn-static"
-    access_modes       = ["ReadWriteOnce"]
-    resources {
-      requests = {
-        storage = local.volumes.foundry_data.size
-      }
-      limits = {
-        storage = local.volumes.foundry_data.size
-      }
-    }
-  }
-}
-
-resource "kubernetes_persistent_volume_claim" "i" {
-  depends_on = [kubernetes_namespace.i]
-  metadata {
-    name      = "${local.app_name_safe}-app"
-    namespace = local.namespace
-    labels    = local.common_labels
-  }
-  spec {
-    storage_class_name = "longhorn-static"
-    access_modes       = ["ReadWriteOnce"]
-    resources {
-      requests = {
-        storage = local.volumes.foundry_app.size
-      }
-      limits = {
-        storage = local.volumes.foundry_app.size
-      }
-    }
   }
 }
 
@@ -118,14 +80,14 @@ resource "kubernetes_deployment" "i" {
           ]
 
           volume_mount {
-            name       = local.volumes.foundry_data.name
-            sub_path   = "syncthing-config"
+            name       = "syncthing-config"
             mount_path = "/var/syncthing/config"
+            sub_path   = "syncthing-config"
           }
 
           volume_mount {
-            name       = local.volumes.foundry_data.name
-            mount_path = "/var/syncthing${local.volumes.foundry_data.path}"
+            name       = "data"
+            mount_path = "/var/syncthing/foundrydata"
           }
         }
 
@@ -151,27 +113,72 @@ resource "kubernetes_deployment" "i" {
             container_port = local.ports.web.port
           }
 
-          volume_mount {
-            mount_path = local.volumes.foundry_data.path
-            name       = local.volumes.foundry_data.name
+          dynamic "volume_mount" {
+            for_each = local.volume_mounts
+            content {
+              name       = volume_mount.value.name
+              mount_path = volume_mount.value.mount_path
+              sub_path   = volume_mount.value.sub_path != "" ? volume_mount.value.sub_path : null
+            }
           }
+        }
 
-          volume_mount {
-            mount_path = local.volumes.foundry_app.path
-            name       = local.volumes.foundry_app.name
+        dynamic "container" {
+          for_each = var.foundry_modules.syncthing ? [1] : []
+          content {
+            name  = "${local.app_name_safe}-syncthing"
+            image = "syncthing/syncthing:latest"
+
+            env {
+              name  = "PUID"
+              value = "1000"
+            }
+            env {
+              name  = "PGID"
+              value = "1000"
+            }
+            env {
+              name  = "TZ"
+              value = "America/Los_Angeles"
+            }
+
+            port {
+              name           = local.ports.syncthing.name
+              container_port = local.ports.syncthing.port
+            }
+
+            port {
+              name           = "sync"
+              container_port = 22000
+            }
+
+            volume_mount {
+              name       = "syncthing-config"
+              sub_path   = "syncthing-config"
+              mount_path = "/var/syncthing/config"
+            }
+
+            volume_mount {
+              name       = "data"
+              mount_path = "/var/syncthing/foundrydata"
+            }
+          }
+        }
+
+        dynamic "volume" {
+          for_each = local.volume_mounts
+          content {
+            name = volume.value.name
+            persistent_volume_claim {
+              claim_name = "kubernetes_persistent_volume_claim.${volume.value.name}.metadata[0].name"
+            }
           }
         }
 
         volume {
-          name = local.volumes.foundry_data.name
+          name = "syncthing-config"
           persistent_volume_claim {
-            claim_name = kubernetes_persistent_volume_claim.o.metadata[0].name
-          }
-        }
-        volume {
-          name = local.volumes.foundry_app.name
-          persistent_volume_claim {
-            claim_name = kubernetes_persistent_volume_claim.i.metadata[0].name
+            claim_name = kubernetes_persistent_volume_claim.syncthing_config.metadata[0].name
           }
         }
 
@@ -181,76 +188,33 @@ resource "kubernetes_deployment" "i" {
             name = image_pull_secrets.value
           }
         }
-
-        container {
-
-
-          name  = "${local.app_name_safe}-syncthing"
-          image = "syncthing/syncthing:latest"
-
-          env {
-            name  = "PUID"
-            value = "1000"
-          }
-          env {
-            name  = "PGID"
-            value = "1000"
-          }
-
-          env {
-            name  = "TZ"
-            value = "America/Los_Angeles"
-          }
-
-          port {
-            name           = local.ports.syncthing.name
-            container_port = local.ports.syncthing.port
-          }
-
-          port {
-            name           = "sync"
-            container_port = 22000
-          }
-
-          volume_mount {
-            name       = local.volumes.foundry_data.name
-            sub_path   = "syncthing-config"
-            mount_path = "/var/syncthing/config"
-          }
-
-          volume_mount {
-            name       = local.volumes.foundry_data.name
-            mount_path = "/var/syncthing${local.volumes.foundry_data.path}"
-          }
-
-        }
-
       }
     }
   }
 }
 
+
 resource "helm_release" "dnd_beyond_rolls" {
   depends_on = [kubernetes_namespace.i]
-  count = var.foundry_modules.dnd_beyond_rolls ? 1 : 0
-  chart = "fvtt-dndbeyond-companion"
+  count      = var.foundry_modules.dnd_beyond_rolls ? 1 : 0
+  chart      = "fvtt-dndbeyond-companion"
   repository = "https://mbround18.github.io/helm-charts/"
-  name  = "${local.app_name_safe}-dndbeyond-rolls"
-  namespace = local.namespace
+  name       = "${local.app_name_safe}-dndbeyond-rolls"
+  namespace  = local.namespace
 
   set {
-      name  = "ingress.enabled"
-      value = "true"
+    name  = "ingress.enabled"
+    value = "true"
   }
 
   set {
-      name  = "ingress.hosts[0].host"
-      value = var.domain_name
+    name  = "ingress.hosts[0].host"
+    value = var.domain_name
   }
 
   set {
-      name  = "ingress.hosts[0].paths[0].path"
-      value = "/dndbeyond-rolls"
+    name  = "ingress.hosts[0].paths[0].path"
+    value = "/dndbeyond-rolls"
   }
 
   set {
